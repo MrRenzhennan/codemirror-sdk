3e39106197f74995a6e1b3667ccd633394c6007b
renzhennan <rzn0725@outlook.com>
2019-02-21 14:28:45 +0800

    -

--- src/index.js
+++ src/index.js
@@ -1980,12 +1980,12 @@ export { OnlineProgramming }
 
 
 // var Online_Programming = new OnlineProgramming({
-// 	id: 'editor-box',
-// 	isPaste: false,
-// 	currentMode: 'html/css/js',
-// 	disabledForSelect: false,
-// 	disabledForInput: true,
-// 	externalLink: {
+// 	id: 'editor-box', //容器ID
+// 	isPaste: false,//是否 禁用粘贴
+// 	currentMode: 'html/css/js',//编辑器 默认 表现形势
+// 	disabledForSelect: false,//是否禁用 select -->是否可以切换 编辑器模式
+// 	disabledForInput: true,//是否禁用 input  -->是否 可以 允许 用户 增加 外部链接
+// 	externalLink: { //支持外部链接
 // 		'js': [
 // 			{
 // 				name: 'vue',
@@ -2021,7 +2021,7 @@ export { OnlineProgramming }
 // 			}
 // 		]
 // 	},
-// 	button: [
+// 	button: [ //底部按钮表现形势
 // 		{
 // 			text: '点击运行',
 // 			style: 'background: #ff0000'
@@ -2032,7 +2032,7 @@ export { OnlineProgramming }
 // 		}
 // 	]
 // });
-
+// //编辑器 模式内容
 // Online_Programming.SetEditorVal(
 // 	[
 // 		{
@@ -2049,24 +2049,25 @@ export { OnlineProgramming }
 // 		}
 // 	]
 // )
-
+// //编辑器 底部 按钮事件注册
 // Online_Programming.EventForButton(
 // 	[
 // 		{
-// 			text: '点击运行',
+// 			text: '点击运行', //这里text 要与上面传入的button text一致
 // 			callback: function () {
 // 				console.log('点击运行');
-// 				let val = Online_Programming.GetEditorVal();
+// 				let val = Online_Programming.GetEditorVal(); //获取编辑器内容
 // 				console.log(val)
-// 				Online_Programming.RequestStyleSetVal(val)
+// 				Online_Programming.RequestStyleSetVal(val) //展示区 展示   注意：不要改变val 的数据格式
 // 			}
 // 		},
 // 		{
 // 			text: '重置编码',
 // 			callback: function () {
 // 				console.log('重置编码');
-// 				Online_Programming.ResetContent();
-// 				Online_Programming.SetEditorVal(
+// 				Online_Programming.ResetContent(); //重置代码
+
+// 				Online_Programming.SetEditorVal(  //重置后 是否要显示 默认值
 // 					[
 // 						{
 // 							key: 'html',
